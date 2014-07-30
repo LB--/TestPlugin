2ab44252407d18e23dab13f1cc6484d0d71819c7
Nicholas "LB" Braden <LB@LB-Stuff.com>
2014-07-29 23:23:40 -0500

    x10 XP drop for testing

--- src/main/java/com/lb_stuff/testplugin/TestPluginPlugin.java
+++ src/main/java/com/lb_stuff/testplugin/TestPluginPlugin.java
@@ -4,6 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockExpEvent;
+import org.bukkit.event.block.BlockBreakEvent;
 
 public class TestPluginPlugin extends JavaPlugin implements Listener
 {
@@ -15,5 +17,21 @@ public class TestPluginPlugin extends JavaPlugin implements Listener
 	@Override
 	public void onDisable()
 	{
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+	public void onXp(BlockExpEvent e)
+	{
+		if(e instanceof BlockBreakEvent)
+		{
+			return;
+		}
+		e.setExpToDrop(e.getExpToDrop()*10);
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+	public void onBreak(BlockBreakEvent e)
+	{
+		e.setExpToDrop(e.getExpToDrop()*10);
 	}
 }
