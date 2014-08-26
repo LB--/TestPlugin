2a75c7f3b7842decb7a287d7ed5d73a21a70ad09
Nicholas "LB" Braden <LB@LB-Stuff.com>
2014-08-26 08:44:17 -0500

    Make example http://forums.bukkit.org/threads/would-anyone-happen-to-have-a-player-database-class.306846/#post-2768882

--- src/main/java/com/lb_stuff/testplugin/TestPluginPlugin.java
+++ src/main/java/com/lb_stuff/testplugin/TestPluginPlugin.java
@@ -1,9 +1,18 @@
 package com.lb_stuff.testplugin;
 
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TestPluginPlugin extends JavaPlugin implements Listener
 {
@@ -15,5 +24,52 @@ public class TestPluginPlugin extends JavaPlugin implements Listener
 	@Override
 	public void onDisable()
 	{
+	}
+
+	private File getPlayerConfig(UUID uuid)
+	{
+		File f = new File(getDataFolder(), "players/");
+		f.mkdirs();
+		f = new File(f, ""+uuid);
+		try
+		{
+			f.createNewFile();
+		}
+		catch(IOException ex)
+		{
+			getLogger().log(Level.SEVERE, null, ex);
+		}
+		return f;
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void onPlayerJoin(PlayerJoinEvent e)
+	{
+		File f = getPlayerConfig(e.getPlayer().getUniqueId());
+		YamlConfiguration conf = YamlConfiguration.loadConfiguration(f);
+		conf.set("last-username", e.getPlayer().getName());
+		try
+		{
+			conf.save(f);
+		}
+		catch(IOException ex)
+		{
+			getLogger().log(Level.SEVERE, null, ex);
+		}
+	}
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerLEave(PlayerQuitEvent e)
+	{
+		File f = getPlayerConfig(e.getPlayer().getUniqueId());
+		YamlConfiguration conf = YamlConfiguration.loadConfiguration(f);
+		conf.set("last-display-name", e.getPlayer().getDisplayName());
+		try
+		{
+			conf.save(f);
+		}
+		catch(IOException ex)
+		{
+			getLogger().log(Level.SEVERE, null, ex);
+		}
 	}
 }
